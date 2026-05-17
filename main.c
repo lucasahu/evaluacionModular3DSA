8ee0a7f30c76088887c8733b203a066789dbc185
Nicolas Jaramillo <nickjcoder@gmail.com>
2026-05-17 11:04:16 -0400

    esqueleto menu mientras tomo cafe

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define MAX_ZONAS 15
 
 /* struct principal */
@@ -26,7 +29,7 @@ struct Entrada {
 struct NodoUsuarioEntrada {
     struct Usuario *datosUsuario;
     struct NodoUsuarioEntrada *sig;
-}
+};
 
 struct NodoUsuario {
     struct Usuario *datosUsuario; /* puntero a los datos del usuario*/
@@ -71,8 +74,76 @@ struct NodoFila {
     struct NodoFila *sig; /* puntero al siguiente nodo de la fila */
 };
 
-/*main de test*/
+void inicializarParque(struct Parque *parque) {
+    int i;
+    /* funcion de inicializado de parque 0 o null hasta input del usuario*/
+    parque->cantidadZonas = 0;
+    parque->visitantesHoy = 0;
+    parque->headUsuarios = NULL;
+    parque->raizEntradas = NULL;
+
+    for (i = 0; i < MAX_ZONAS; i++) {
+        parque->zonas[i] = NULL;
+    }
+}
+
+void menuIBCLandia(struct Parque *parque) {
+    int num;
+
+    do {
+        printf("Bienvenido al menu!\n");
+        printf("1. Menu de usuarios\n");
+        printf("2. Menu de entradas\n");
+        printf("3. Menu de zonas\n");
+        printf("4. Menu de atracciones\n");
+        printf("5. Menu de filas\n");
+        printf("6. Menu de reportes\n");
+        printf("7. Salir del menu\n");
+        printf("Porfavor digite una opcion valida del menu: ");
+
+        scanf(" %d", &num);
+
+        switch (num) {
+            default:
+                printf("opcion no valida, mejor suerte a la proxima!");
+                break;
+            case 1:
+                printf("usuarios no listo aun");
+                break;
+            case 2:
+                printf("entradas no listo aun");
+                break;
+            case 3:
+                printf("zonas no listo aun");
+                break;
+            case 4:
+                printf("atracciones no listo aun");
+                break;
+            case 5:
+                printf("filas no listo aun");
+                break;
+            case 6:
+                printf("reportes no disponibles");
+                break;
+            case 7:
+                printf("Esperamos hayas disfrutado de IBCLandia!");
+                break;
+        }
+    }while (num != 7);
+}
+
+
 int main(void) {
-    printf("test\n");
+    struct Parque *parque;
+    parque = (struct Parque *)malloc(sizeof(struct Parque))
+
+    if (parque == NULL) {
+        printf("No se pudo crear el parque.");
+        return 0;
+    }
+
+    inicializarParque(parque);
+    menuIBCLandia(parque);
+
     return 0;
 }
